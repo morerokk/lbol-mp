4cdca8f84295dfa380a927c1fa92ee779c10fb09
morerokk <hugo.zink@hotmail.com>
2026-08-23 15:18:13 +0200

    chore: bump version after moon rabbits

--- LBOLMP/MpInfo.cs
+++ LBOLMP/MpInfo.cs
@@ -7,7 +7,7 @@
         /// <summary>
         /// Mod version for LBOL MP.
         /// </summary>
-        public const string Version = "0.10.2";
+        public const string Version = "0.10.3";
 
         /// <summary>
         /// The current networking protocol version. Only clients with the same versions can join each other.
