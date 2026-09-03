0c4165fddf61b25e93d434a32b3a66a37c7e18e2
morerokk <hugo.zink@hotmail.com>
2026-09-03 12:34:04 +0200

    chore: bump version

--- LBOLMP/MpInfo.cs
+++ LBOLMP/MpInfo.cs
@@ -7,7 +7,7 @@
         /// <summary>
         /// Mod version for LBOL MP.
         /// </summary>
-        public const string Version = "0.11.13";
+        public const string Version = "0.11.14";
 
         /// <summary>
         /// The current networking protocol version. Only clients with the same versions can join each other.
